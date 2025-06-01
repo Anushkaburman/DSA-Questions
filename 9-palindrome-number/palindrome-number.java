2d9f7b6ddabf8e71cefb2bc9a76b1a0974c1a6fc
Anushkaburman <anushkaburman467@gmail.com>
2025-06-02 00:30:17 +0530

    Time: 5 ms (69.99%) | Memory: 43.4 MB (99.57%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -6,15 +6,10 @@ class Solution {
        long reverse = 0;
         long temp = x;
         while(temp>0){
-            long r = temp%10;
-            reverse = reverse *10+r;
+            reverse = reverse *10+(temp%10);
             temp = temp/10;
         }
-        if(x==reverse){
-            return true; 
-        }
-        else{
-        return false;
-        } 
+      return reverse==x; 
+        
     }
 }
