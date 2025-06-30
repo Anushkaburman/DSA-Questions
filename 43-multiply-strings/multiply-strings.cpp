367f11454f76a4a819fa8063bb4986dfc0f1e08c
Anushkaburman <anushkaburman467@gmail.com>
2025-07-01 02:06:11 +0530

    Time: 1 ms (77.52%) | Memory: 9.6 MB (59.96%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        if(num1 == "0" || num2 == "0"){
+            return "0";
+        }
+        int n = num1.size();
+        int m = num2.size();
+        vector<int>v(n+m,0);
+        for(int i=n-1; i>=0; --i){
+            for(int j=m-1; j>=0; --j){
+                int mul = (num1[i] - '0')*(num2[j] - '0');
+                int sum = mul + v[i+j+1];
+
+                v[i+j+1] = sum%10;
+                v[i+j] += sum/10;
+            }
+        }
+        string st;
+        for(int val: v){
+            if(!(st.empty() && val==0)){
+                st.push_back(val + '0');
+            }
+        }
+        return st.empty()?"0":st;
+         
+    }
+};
