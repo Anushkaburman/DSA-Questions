63fd7cce18e6d4a72a17c4b25f43e357502c57c4
Anushkaburman <anushkaburman467@gmail.com>
2025-07-24 02:56:35 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (75.33%) - LeetSync

---
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool Binary(vector<int> &nums1,int target){
+    
+        int lo = 0;
+        int hi = nums1.size()-1;
+        while(lo<=hi){
+            int mid = lo + (hi - lo)/2;
+            if(nums1[mid]==target){
+                nums1.erase(nums1.begin()+mid);
+                return true;
+            }else if(nums1[mid] < target){
+                lo = mid + 1;
+            }else{
+                hi = mid - 1;
+            }
+        }
+        return false;
+    }
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        if(nums1.size()>nums2.size()){
+            swap(nums1,nums2);
+        }
+        sort(nums2.begin(),nums2.end());
+        vector<int>s;
+        for(int num : nums1){
+            if(Binary(nums2,num)){
+                s.push_back(num);
+            }
+        }
+        return s;
+
+    }
+};
