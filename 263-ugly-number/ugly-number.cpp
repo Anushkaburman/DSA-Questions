43dc395900c4adba729dc6ae0e223faec0f064f7
Anushkaburman <anushkaburman467@gmail.com>
2025-07-31 00:48:12 +0530

    Time: 3 ms (4.86%) | Memory: 8.3 MB (11.05%) - LeetSync

---
+++ 263-ugly-number/ugly-number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        if(n<=0) return false;
+        vector<int>arr;
+        while(n%2==0){
+            arr.push_back(2);
+            n = n/2;
+        }
+        for(int i = 3; 1LL* i*i<=n; i+=2){
+            while(n%i == 0){
+                arr.push_back(i);
+                n = n/i;
+            }
+            
+        }
+        if(n>2){
+            arr.push_back(n);
+        }
+        for(int num : arr){
+            if((num != 2) && (num != 3) && (num != 5)){
+                return false;
+            }
+        }
+        return true;
+    }
+};
