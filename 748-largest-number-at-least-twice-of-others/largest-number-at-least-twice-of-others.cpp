c88e873ab1d698bd04668c3301c956a8f955cadc
Anushkaburman <anushkaburman467@gmail.com>
2026-02-22 17:48:50 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (78.57%) - LeetSync

---
+++ 748-largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int dominantIndex(vector<int>& nums) {
+        int max = -1;
+        int secondMax = -1;
+        int index = -1;
+
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]>max){
+                secondMax = max;
+                max = nums[i];
+                index = i;
+            }else if(nums[i]>secondMax){
+                secondMax = nums[i];
+            }
+        }
+        if(max >= 2*secondMax){
+            return index;
+        }
+        return -1;
+    }
+};
