eab0c9ca02cba39a27e3332ac6f4d6fd86bde20c
Anushkaburman <anushkaburman467@gmail.com>
2026-02-24 16:36:32 +0530

    Time: 4 ms (19.86%) | Memory: 28.2 MB (27.43%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+         int king = 0;
+         int vote = 0;
+
+        for(int n: nums){
+            if(vote == 0){
+                king = n;
+            }
+            if(king == n){
+                vote++;
+            }
+            else{
+                vote--;
+            }
+        }
+        return king;        
+    }
+};
