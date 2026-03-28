b3240bfe71bad8ffa449657668e891b909000499
Anushkaburman <anushkaburman467@gmail.com>
2026-03-29 00:30:30 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (55.10%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int low = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]!=0){
+                nums[low] = nums[i];
+                low++;
+            }
+        }
+        for(int i=low; i<nums.size(); i++){
+            nums[i]=0;
+        }
+    }
+};
