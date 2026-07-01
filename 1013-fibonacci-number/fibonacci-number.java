ff91045eeb426c9c9c01128b8d3ee2d3efdfe160
Anushkaburman <anushkaburman467@gmail.com>
2026-07-01 17:24:37 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (69.53%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -3,15 +3,32 @@ class Solution {
         if(n<=1){
             return n;
         }
-        int dp[] = new int[n+1];
-        dp[0] = 0;
-        dp[1] = 1;
+        int first = 0;
+        int second = 1;
+        int third = 0;
         for(int i=2; i<=n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            third = first + second;
+            first = second;
+            second = third;
         }
-        return dp[n];
+        return third;
      }
 }
+
+// class Solution {
+//      public int fib(int n) {
+//         if(n<=1){
+//             return n;
+//         }
+//         int dp[] = new int[n+1];
+//         dp[0] = 0;
+//         dp[1] = 1;
+//         for(int i=2; i<=n; i++){
+//             dp[i] = dp[i-1] + dp[i-2];
+//         }
+//         return dp[n];
+//      }
+// }
 
 // class Solution {
 //      public int fib(int n) {
