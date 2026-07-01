a3078a15da58291df1680f18e9896e6ec4402f1d
Anushkaburman <anushkaburman467@gmail.com>
2026-07-01 16:56:21 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (15.73%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,9 +1,29 @@
 class Solution {
-    public int fib(int n) {
-         if(n<2){
+     public int fib(int n) {
+        int dp[] =new int[n+1];
+        Arrays.fill(dp,-1);
+        return helper(n,dp);
+     }
+
+     int helper(int n, int[] dp){
+        if(n==0 || n==1){
             return n;
-         }
-        return fib(n-1) + fib(n-2);
-         
-    }
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        int first = helper(n-1,dp);
+        int second = helper(n-2,dp);
+        dp[n] = first + second;
+        return dp[n];
+     }
 }
+// class Solution {
+//     public int fib(int n) {
+//          if(n<2){
+//             return n;
+//          }
+//         return fib(n-1) + fib(n-2);
+         
+//     }
+// }
