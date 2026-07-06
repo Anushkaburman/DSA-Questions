fcda1d25765d52fb832da647fdf57e06f2dcdc26
Anushkaburman <anushkaburman467@gmail.com>
2026-07-06 17:22:01 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (87.68%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,31 +1,64 @@
-// class Solution {
-//     public int rob(int[] nums) {
-
-//     }
-// }
 class Solution {
     public int rob(int[] nums) {
+        
         int n = nums.length;
-        int dp[] = new int[n+1];
-        Arrays.fill(dp,-1);
-        return helper(0,nums,dp);
-
-    }
-    int helper(int index, int []nums, int []dp){
-        if(index>=nums.length){
-            return 0;
+        if(n==1){
+            return nums[0];
         }
-
-        if(dp[index]!=-1){
-            return dp[index];
+        int first = 0;
+        int second = nums[n-1];
+        int current = 0;
+        for(int i=n-2; i>=0; i--){
+            int rob = nums[i] + first;
+            int skip = second;
+            current = Math.max(rob,skip);
+            first = second;
+            second = current;
         }
-        int rob = nums[index] + helper(index + 2,nums,dp);
-        int skip = helper(index + 1,nums,dp);
-        int max = Math.max(rob,skip);
-        dp[index] = max;
-        return dp[index]; 
+        return current;
     }
 }
+
+
+
+// class Solution {
+//     public int rob(int[] nums) {
+//         int n = nums.length;
+//         int dp[] = new int[n+1];
+//         dp[n] = 0;
+//         dp[n-1] = nums[n-1];
+//         for(int i = n-2; i>=0; i--){
+//             int rob = nums[i] + dp[i+2];
+//             int skip = dp[i+1];
+//             int max = Math.max(skip,rob);
+//             dp[i] = max;
+//         }
+//     return dp[0];
+//     }
+//}
+// class Solution {
+//     public int rob(int[] nums) {
+//         int n = nums.length;
+//         int dp[] = new int[n+1];
+//         Arrays.fill(dp,-1);
+//         return helper(0,nums,dp);
+
+//     }
+//     int helper(int index, int []nums, int []dp){
+//         if(index>=nums.length){
+//             return 0;
+//         }
+
+//         if(dp[index]!=-1){
+//             return dp[index];
+//         }
+//         int rob = nums[index] + helper(index + 2,nums,dp);
+//         int skip = helper(index + 1,nums,dp);
+//         int max = Math.max(rob,skip);
+//         dp[index] = max;
+//         return dp[index]; 
+//     }
+// }
 
 // class Solution {
 //     public int rob(int[] nums) {
