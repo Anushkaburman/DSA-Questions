2ee5a3878d8a1e9891c688bb51c710064bccb8a8
Anushkaburman <anushkaburman467@gmail.com>
2026-07-07 17:18:13 +0530

    Time: 37 ms (72.33%) | Memory: 45.9 MB (82.62%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -0,0 +1,37 @@
+//Approach -3 tabulation 
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int dp[] = new int[n+1];
+        Arrays.fill(dp,1);
+        for(int current = 1; current<n; current++){
+            for(int prev = 0; prev<current; prev++){
+                if(nums[prev]<nums[current]){
+                    dp[current] = Math.max(dp[current],dp[prev]+1);
+                }
+            }
+        }
+       int ans = 1;
+       for(int val : dp){
+        ans = Math.max(val,ans);
+       }
+       return ans;
+    }
+}
+// class Solution {
+//     public int lengthOfLIS(int[] nums) {
+//         return helper(0,-1,nums);
+//     }
+//     int helper(int currIndex , int prevIndex,int nums[]){
+//         if(i==nums.length){
+//             return 0;
+//         }
+
+//         int skip = helper(currIndex + ,prevIndex, nums);
+//         int include = 0;
+//         if(prevIndex == -1 || nums[currIndex]>nums[prevIndex]]{
+//             include = 1 + (currIndex + 1,currIndex,nums);
+//         }
+//        return Math.max(skip,inlude,;) 
+//     }
+// }
