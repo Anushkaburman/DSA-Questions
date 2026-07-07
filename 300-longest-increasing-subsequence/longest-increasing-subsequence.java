f8106f4f6c85c99b35a8db781a938bd52e22f6e3
Anushkaburman <anushkaburman467@gmail.com>
2026-07-07 17:37:18 +0530

    Time: 2 ms (100.00%) | Memory: 46.3 MB (32.47%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,23 +1,53 @@
-//Approach -3 tabulation 
+//space optimisation
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        int dp[] = new int[n+1];
-        Arrays.fill(dp,1);
-        for(int current = 1; current<n; current++){
-            for(int prev = 0; prev<current; prev++){
-                if(nums[prev]<nums[current]){
-                    dp[current] = Math.max(dp[current],dp[prev]+1);
+        int[] tails = new int[nums.length];
+        int tailSize = 0;
+        for(int num : nums){
+            int low = 0;
+            int high = tailSize;
+            while(low < high){
+                int mid = (low + high)/2;
+                if(tails[mid]<num){
+                    low = mid + 1;
+                }
+                else{
+                    high = mid;
                 }
             }
+            tails[low] = num;
+            if(low == tailSize){
+                tailSize++;
+            }
         }
-       int ans = 1;
-       for(int val : dp){
-        ans = Math.max(val,ans);
-       }
-       return ans;
+        return tailSize;
     }
 }
+
+
+
+
+
+//Approach -3 tabulation 
+// class Solution {
+//     public int lengthOfLIS(int[] nums) {
+//         int n = nums.length;
+//         int dp[] = new int[n+1];
+//         Arrays.fill(dp,1);
+//         for(int current = 1; current<n; current++){
+//             for(int prev = 0; prev<current; prev++){
+//                 if(nums[prev]<nums[current]){
+//                     dp[current] = Math.max(dp[current],dp[prev]+1);
+//                 }
+//             }
+//         }
+//        int ans = 1;
+//        for(int val : dp){
+//         ans = Math.max(val,ans);
+//        }
+//        return ans;
+//     }
+// }
 // class Solution {
 //     public int lengthOfLIS(int[] nums) {
 //         return helper(0,-1,nums);
